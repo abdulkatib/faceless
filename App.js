4c190b2e4d0c492795ca8c63a58828512b6a77c6
Abdul katib <mrabdo16th@gmail.com>
2020-08-15 00:02:00 -0400

    wip add messages list with delete

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React from "react";
-import Card from "./app/components/Card";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return (
-    <>
-      <Card
-        title={"Abdul"}
-        subTitle={"Hi :)"}
-        image={require("./app/assets/image.jpg")}
-      />
-      <Card
-        title={"Moe"}
-        subTitle={"What's up"}
-        image={require("./app/assets/image.jpg")}
-      />
-    </>
-  );
+  return <MessagesScreen />;
 }
