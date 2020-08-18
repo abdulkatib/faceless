032baffdb737ded1a4e75cec6019ae5636a5a037
Abdul katib <mrabdo16th@gmail.com>
2020-08-18 00:21:59 -0400

    wip

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
-import Screen from "./app/components/Screen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./app/navigation/AuthNavigation";
+
 import navigationTheme from "./app/navigation/navigationTheme";
+import AuthNavigator from "./app/navigation/AuthNavigation";
 import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
+      <AuthNavigator />
+      {/* <AppNavigator /> */}
     </NavigationContainer>
   );
 }
